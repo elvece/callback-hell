b3b1b45acee67063f3c796bf00bb336467a5a596
elvece <lvcifferello@gmail.com>
2015-10-29 15:06:37 -0600

    ajax request working, pseudo coded rest:

--- client/js/main.js
+++ client/js/main.js
@@ -1,13 +1,5 @@
-// dependencies
-var csv = require('csv');
-var fs = require('fs');
-var request=require("request");
-var cheerio=require("cheerio");
-var writeStream = fs.createWriteStream("file.csv");
+// add scripts
 
-var request = require('request');
-request('http://www.google.com', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body) // Show the HTML for the Google homepage.
-  }
-})
+$(document).on('ready', function() {
+  console.log('sanity check!');
+});
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var request=require("request");
+var cheerio=require("cheerio");
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/hell', function(req, res, next) {
+  //ajax request
+  var url = 'https://news.ycombinator.com/';
+  var html = request(url, function (error, response, html){
+    //parse html
+    var $ = cheerio.load(html);
+    var title = $('td.title a').first().text();
+
+    //is javascript in the title
+    var hasJavascript = title.match('javascript');
+    console.log('line 24', hasJavascript);
+
+    if (!hasJavascript) {
+      //request python.org, return something fun
+    } else {
+      //request reddit, parsre, test if javascript is part of the title
+      //if yes
+        //request mdn, return something fun
+      //if no
+        //request puthon.org, return something fun
+    }
+  });
 });
+
+router.get('/hellway', function(req, res, next){
+  request('https://www.reddit.com/r/Web_Development/', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      // console.log('Reddit:' + body)
+    }
+  });
+});
+//value of the first a with the first class of title
 
 module.exports = router;
