7e29592f870de5e967ff4e7958b17bb2d2c17cf1
elvece <lvcifferello@gmail.com>
2015-10-29 16:13:57 -0600

    refactored with callback

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,59 +4,111 @@ var fs = require('fs');
 var request=require("request");
 var cheerio=require("cheerio");
 
-router.get('/hell', function(req, res, next) {
-  //ajax request
-  var newUrl = 'https://news.ycombinator.com/';
-  var newsHtml = request(newUrl, function (error, response, html){
+var urls = {
+  hacker: 'https://news.ycombinator.com/',
+  reddit: 'https://www.reddit.com/r/Web_Development/',
+  python: 'https://python.org/',
+  mozilla: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
+};
+
+function hasJavascript(url, selector, cb){
+  //get html
+  request(url, function(err, reponse, html){
     //parse html
     var $ = cheerio.load(html);
-    var newsTitle = $('td.title a').first().text();
+    var title = $(selector).first().text();
 
-    //is javascript in the title
-    var newsHasJavascript = newsTitle.match('javascript');
-    // console.log('line 24', hasJavascript);
-
-    if (!newsHasJavascript) {
-      //request python.org, return something fun
-      var pythonUrl = 'https://python.org';
-      var pythonHtml = request(pythonUrl, function(err, response, html){
-
-        var $ = cheerio.load(html);
-        var pythonLogo = $('img').attr('src');
-        res.send('<img src="https://python.org/'+pythonLogo+'">');
-
-
-      });
-
+    //is javascript in the title?
+    if (title.match('javascript')){
+      cb(null, true);
     } else {
-      //request reddit, parsre, test if javascript is part of the title
-      //if yes
-        //request mdn, return something fun
-      //if no
-        //request puthon.org, return something fun
-
-      //ajax request
-      var redditUrl = 'https://www.reddit.com/r/Web_Development/';
-      var redditHtml = request(redditUrl, function(err, response, html){
-        //parse html
-        var $ = cheerio.load(html);
-        var redditTitle = $('a.title').first().text();
-
-        //is javascript in the title
-        var redditHasJavascript = redditTitle.match('javascript');
-        // console.log('right here', redditHasJavascript);
-      });
+      cb(null, false);
     }
   });
-});
+}
 
-router.get('/hellway', function(req, res, next){
-  request('https://www.reddit.com/r/Web_Development/', function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      // console.log('Reddit:' + body)
+router.get('/hell', function(req, res, next) {
+
+  var test = hasJavascript(url.hacker, 'td.title a', function(err, results){
+    if (err) {
+      console.log('something went wrong');
     }
+      res.send(results);
   });
+
+
+
+
+//   //ajax request
+//   var newsUrl = 'https://news.ycombinator.com/';
+//   var newsHtml = request(newsUrl, function (error, response, html){
+//     //parse html
+//     var $ = cheerio.load(html);
+//     var newsTitle = $('td.title a').first().text();
+
+//     //is javascript in the title
+//     var newsHasJavascript = newsTitle.match('javascript');
+//     // console.log('line 24', hasJavascript);
+
+//     if (!newsHasJavascript) {
+//       //request python.org, return something fun
+//       var pythonUrl = 'https://python.org';
+//       var pythonHtml = request(pythonUrl, function(err, response, html){
+
+//         var $ = cheerio.load(html);
+//         var pythonLogo = $('img').attr('src');
+//         res.send('<img src="https://python.org/'+pythonLogo+'">');
+
+
+//       });
+
+//     } else {
+//       //request reddit, parsre, test if javascript is part of the title
+//       //if yes
+//         //request mdn, return something fun
+//       //if no
+//         //request puthon.org, return something fun
+
+//       //ajax request
+//       var redditUrl = 'https://www.reddit.com/r/Web_Development/';
+//       var redditHtml = request(redditUrl, function(err, response, html){
+//         //parse html
+//         var $ = cheerio.load(html);
+//         var redditTitle = $('a.title').first().text();
+
+//         //is javascript in the title
+//         var redditHasJavascript = redditTitle.match('javascript');
+//         // console.log('right here', redditHasJavascript);
+
+//         if (!redditHasJavascript){
+//           //request python.org, return something fun
+//           var pythonUrl = 'https://python.org';
+//           var pythonHtml = request(pythonUrl, function(err, response, html){
+
+//             //parse html
+//             var $ = cheerio.load(html);
+//             var pythonLogo = $('img').attr('src');
+//             res.send('<img src="https://python.org/'+pythonLogo+'">');
+//         });
+//       } else {
+//         var mdnUrl = 'https://developer.mozilla.org/en-US/docs/Web/JavaScript';
+//         var mdnHtml = request(mdnUrl, function(err, response, html){
+//           var $ = cheerio.load(html);
+//           var tab = $('#tabzilla');
+//           res.send(tab);
+//         });
+//       }
+//     });
+//     }
+//   });
+// });
+
+// router.get('/hellway', function(req, res, next){
+//   request('https://www.reddit.com/r/Web_Development/', function (error, response, body) {
+//     if (!error && response.statusCode == 200) {
+//       // console.log('Reddit:' + body)
+//     }
+//   });
 });
-//value of the first a with the first class of title
 
 module.exports = router;
